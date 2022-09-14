f31a58f226523de549480bc30e24daa72604c5bc
OlesyaY <103993684+OlesyaY@users.noreply.github.com>
2022-09-14 10:39:39 +0300

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const handleChangeBgColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
+
+const handleStart = () => {
+  startBtn.setAttribute('disabled', true);
+
+  handleChangeBgColor();
+  timerId = setInterval(handleChangeBgColor, 1000);
+};
+
+const handleStop = () => {
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+};
+
+startBtn.addEventListener('click', handleStart);
+stoptBtn.addEventListener('click', handleStop);
